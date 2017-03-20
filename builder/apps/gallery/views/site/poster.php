212fd3ff0babc9faea7b7fbfbf7aae42e1b69fa7
Milos Dodic <milos_dodic@live.com>
2017-03-20 21:19:26 +0100

    builder gallery poster view added

---
+++ builder/apps/gallery/views/site/poster.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d0dic
+ * Date: 20.3.17.
+ * Time: 21.15
+ */
+?>
+
+<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+    <div class="thumbnail">
+        <a href="<?= \yii\helpers\Url::to(['site/preview', 'id' => $model->id]) ?>">
+            <img src="<?= Yii::$app->request->baseUrl ?>/static/posters/<?= $model->image ?>"
+                 alt="<?= $model->image ?>"></a>
+        <div class="caption">
+            <h3><?= $model->name ?> <small>@<i><?= $model->owner->name ?></i></small></h3>
+            <p class="text-right">
+
+                <a class="btn btn-danger" href="<?= \yii\helpers\Url::to(['site/like', 'id' => $model->id]) ?>">
+                    Like <span class="glyphicon glyphicon-thumbs-up"></span>
+                </a>
+                <a class="btn btn-primary" href="javascript:share(<?= $model->id ?>)">
+                    Share <span class="glyphicon glyphicon-share"></span>
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
